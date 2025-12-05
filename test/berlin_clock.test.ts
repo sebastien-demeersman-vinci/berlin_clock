d97e4cf3cfd19e5c33d887ee873143d18688ad88
colin-techy-vinci <ctechy@DESKTOP-2O4C5NO.localdomain>
2025-12-05 16:49:03 +0100

    Add: test for second odd and pair

---
+++ test/berlin_clock.test.ts
@@ -0,0 +1,8 @@
+describe('Berlin Clock', () => {
+    it("should return 1 for seconds pair", () => {
+        expect(1).toBe('1');
+    });
+    it("should return 0 for seconds odd", () => {
+        expect(1).toBe('1');
+    });
+});
