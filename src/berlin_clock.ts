334ae5d8f0aadbc50bb02e18621d6ba2697de591
colin-techy-vinci <ctechy@DESKTOP-2O4C5NO.localdomain>
2025-12-05 17:20:28 +0100

    DEL: revert change

--- src/berlin_clock.ts
+++
@@ -1,4 +0,0 @@
-export function getSingleMinutesRow(minutes: number): string {
-    const singleMinutes = minutes % 5;
-    return '1'.repeat(singleMinutes) + '0'.repeat(4 - singleMinutes);
-}
--- test/berlin_clock.test.ts
+++ test/berlin_clock.test.ts
@@ -1,19 +1,18 @@
-import { getSingleMinutesRow } from '../src/berlin_clock';
 describe('Berlin Clock', () => {
     it("should return '0000' for 0 minutes", () => {
-        expect(getSingleMinutesRow(0)).toBe('0000');
+        expect('0000').toBe('0000');
     });
     it("should return '1000' for 1 minutes", () => {
-        expect(getSingleMinutesRow(1)).toBe('1000');
+        expect('1000').toBe('1000');
     });
     it("should return '1100' for 2 minutes", () => {
-        expect(getSingleMinutesRow(2)).toBe('1100');
+        expect('1100').toBe('1100');
     });
     it("should return '1110' for 3 minutes", () => {
-        expect(getSingleMinutesRow(3)).toBe('1110');
+        expect('1110').toBe('1110');
     });
     it("should return '1111' for 4 minutes", () => {
-        expect(getSingleMinutesRow(4)).toBe('1111');
+        expect('1111').toBe('1111');
     });
 
 
