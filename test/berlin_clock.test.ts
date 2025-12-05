7e340195b743de06120166af6cd8ca49fe6434f5
colin-techy-vinci <ctechy@DESKTOP-2O4C5NO.localdomain>
2025-12-05 17:01:38 +0100

    Add: test for the single minutes

--- test/berlin_clock.test.ts
+++ test/berlin_clock.test.ts
@@ -1,8 +1,25 @@
 describe('Berlin Clock', () => {
-    it("should return 1 for seconds pair", () => {
+    it("should return '0000' for 0 minutes", () => {
+        expect('0000').toBe('0000');
+    });
+    it("should return '1000' for 1 minutes", () => {
+        expect('1000').toBe('1000');
+    });
+    it("should return '1100' for 2 minutes", () => {
+        expect('1100').toBe('1000');
+    });
+    it("should return '1110' for 3 minutes", () => {
+        expect('1110').toBe('1000');
+    });
+    it("should return '1111' for 4 minutes", () => {
+        expect('1111').toBe('1111');
+    });
+
+
+    /*it("should return 1 for seconds pair", () => {
         expect('1').toBe('1');
     });
     it("should return 0 for seconds odd", () => {
         expect('0').toBe('0');
-    });
+    });*/
 });
