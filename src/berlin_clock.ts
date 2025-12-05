4f8be6e8da9aa006ccd061a3ff2d770f76d7693b
colin-techy-vinci <ctechy@DESKTOP-2O4C5NO.localdomain>
2025-12-05 17:26:18 +0100

    ADD: add test for 1 minutes

--- src/berlin_clock.ts
+++ src/berlin_clock.ts
@@ -1,4 +1,6 @@
 export function getSingleMinutesRow(minutes: number): string {
     const singleMinutes = minutes;
+    if (singleMinutes > 1)
+        return '1000';
     return '0000';
-}
+};
--- test/berlin_clock.test.ts
+++ test/berlin_clock.test.ts
@@ -3,10 +3,10 @@ describe('Berlin Clock', () => {
     it("should return '0000' for 0 minutes", () => {
         expect(getSingleMinutesRow(0)).toBe('0000');
     });
-    /*it("should return '1000' for 1 minutes", () => {
-        expect('1000').toBe('1000');
+    it("should return '1000' for 1 minutes", () => {
+        expect(getSingleMinutesRow(1)).toBe('1000');
     });
-    it("should return '1100' for 2 minutes", () => {
+    /*it("should return '1100' for 2 minutes", () => {
         expect('1100').toBe('1100');
     });
     it("should return '1110' for 3 minutes", () => {
